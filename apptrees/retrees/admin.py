f0743c30a394027be76b24ab36894201a025ad37
Andrey <brainsgoo@gmail.com>
2024-05-12 12:52:41 +0300

    Дерево

---
+++ apptrees/retrees/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import Post, Category
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class CategoryAdmin(MPTTModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Category, CategoryAdmin)
